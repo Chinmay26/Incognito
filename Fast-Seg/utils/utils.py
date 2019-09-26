eac5c2849c5bc95d0bf5ca93ffb96a9132f6f408
Chinmay Name <chin.naik26@gmail.com>
2019-09-26 11:30:02 -0700

    Add helper functions to visualize images

---
+++ Fast-Seg/utils/utils.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+
+def visualize(**images):
+    """PLot images in one row."""
+    n = len(images)
+    plt.figure(figsize=(16, 5))
+    for i, (name, image) in enumerate(images.items()):
+        plt.subplot(1, n, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(' '.join(name.split('_')).title())
+        plt.imshow(image)
+    plt.show()
+    
+# helper function for data visualization    
+def denormalize(x):
+    """Scale image to range 0..1 for correct plot"""
+    x_max = np.percentile(x, 98)
+    x_min = np.percentile(x, 2)    
+    x = (x - x_min) / (x_max - x_min)
+    x = x.clip(0, 1)
+    return x
