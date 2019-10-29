7f853f2854e2377db5ebd3ea3ef5eaa4b7fc544a
Chinmay Name <chin.naik26@gmail.com>
2019-10-28 23:01:10 -0700

    Add helper to evaluate model total flops & trainable params

--- Fast-Seg/evaluate.py
+++ Fast-Seg/evaluate.py
@@ -1,3 +1,6 @@
+import tensorflow as tf
+import keras.backend as K
+
 """
 
 Evaluate
@@ -16,3 +19,22 @@ class Evaluation(object):
 		if params["model_type"] == 'keras_model':
 			self.graph = params["model_graph_file_path"]
 			self.weights = params["model_weights_file_path"]
+
+
+	def get_model_params(model):
+	    run_meta = tf.RunMetadata()
+
+	    #Get FLOPS
+		opts = tf.profiler.ProfileOptionBuilder.float_operation()    
+		flops = tf.profiler.profile(graph=K.get_session().graph, run_meta=run_meta, 
+									cmd='op', options=opts)
+
+		#Get trainable model parameters
+		opts = tf.profiler.ProfileOptionBuilder.trainable_variables_parameter()    
+		trainable_params = tf.profiler.profile(graph=K.get_session().graph, run_meta=run_meta, 
+									cmd='op', options=opts)
+
+
+	    params={'total_parameters': trainable_params.total_parameters, 'flops': flops.total_float_ops}
+
+
